a4d447968797dde8dbeb67ca1c7070348429a977
xiaojiebudai <1069194334@qq.com>
2017-06-02 16:42:43 +0800

    地图蓝点

--- app/src/main/java/com/sharebicycle/fragment/BaiduMapFragment.java
+++ app/src/main/java/com/sharebicycle/fragment/BaiduMapFragment.java
@@ -69,6 +69,18 @@ public class BaiduMapFragment extends FatherFragment {
             mUiSettings.setZoomControlsEnabled(false);
             mUiSettings.setCompassEnabled(true);
 //            mUiSettings.setMyLocationButtonEnabled(true); //显示默认的定位按钮
+            MyLocationStyle myLocationStyle;
+            myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类
+          myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+            myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+            myLocationStyle.myLocationIcon(BitmapDescriptorFactory.fromBitmap(BitmapFactory
+                    .decodeResource(getResources(), R.mipmap.arp)));
+            myLocationStyle.strokeWidth(5);
+            myLocationStyle. strokeColor(getResources().getColor(R.color.color_main));//设置定位蓝点精度圆圈的边框颜色的方法。
+            myLocationStyle .radiusFillColor(getResources().getColor(R.color.transparent));//设置定位蓝点精度圆圈的填充颜色的方法。
+            aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+//aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
+            aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
 
             aMap.setTrafficEnabled(true);// 显示实时交通状况
             //地图模式可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
@@ -248,7 +260,7 @@ public class BaiduMapFragment extends FatherFragment {
                     LatLng newLatLng =new LatLng(location.getLatitude(),location.getLongitude());
                     changeCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(
                             newLatLng, 17, 30, 0)));
-                    addmark(location.getLatitude(),location.getLongitude());
+//                    addmark(location.getLatitude(),location.getLongitude());
                     stopLocation();
 //                    if(isFirstLatLng){
 //                        //记录第一次的定位信息
